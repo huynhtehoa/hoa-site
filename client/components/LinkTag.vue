ae673d4bd7d05f53b8a4449119561e1064b38359
hoa huynh <huynhtehoa@gmail.com>
2022-08-07 22:36:41 +0700

    feat(link tag): add LinkTag component To reuse html a tag easier

---
+++ client/components/LinkTag.vue
@@ -0,0 +1,18 @@
+<!-- Custom html a tag for reusability -->
+<template>
+  <a
+    :href="url"
+    target="_blank"
+    rel="noreferrer noopener"
+    class="text-emerald-600 decoration-emerald-400 underline"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  url: string,
+  text: string
+}>();
+</script>
