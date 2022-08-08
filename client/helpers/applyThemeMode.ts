fd3c572c34814ed5b2599ee5857cd31f8008db72
hoa huynh <huynhtehoa@gmail.com>
2022-08-09 00:07:27 +0700

    fix(dark mode): fix unhandled logic

--- client/helpers/applyThemeMode.ts
+++ client/helpers/applyThemeMode.ts
@@ -1,7 +1,7 @@
 
 export default function () {
   const currentHour = new Date().getHours();
-  if (currentHour >= 19) {
+  if (currentHour >= 19 || currentHour <= 6) {
     document.querySelector(':root').className += 'dark';
   } else {
     document.querySelector(':root').className += 'light';
