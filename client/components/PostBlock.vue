29f9cff40aca0ea61213f78f5a41115c560dcbc4
hoa huynh <huynhtehoa@gmail.com>
2022-08-08 23:06:47 +0700

    feat(PostBlock): init post block component Init without styling, to render posts on /posts route

---
+++ client/components/PostBlock.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="text-left align-items center"
+    @click="navigateToPost"
+  >
+    <img
+      :src="post.image.src"
+      :alt="post.image.alt"
+      height="400"
+      width="400"
+    >
+    <div>
+      {{ post.title }}
+    </div>
+    <div>
+      {{ post.description }}
+    </div>
+    <div>
+      {{ standardizeDate(post.date) }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { navigateTo } from '#imports';
+import standardizeDate from '~/helpers/standardizeDate';
+
+import type Post from '~/types/Post';
+
+const props = defineProps<{
+  post: Post
+}>();
+
+const navigateToPost = () => {
+  navigateTo(props.post._path);
+};
+</script>
